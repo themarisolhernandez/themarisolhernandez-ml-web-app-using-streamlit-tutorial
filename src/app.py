3eb4267bc178f3ba9a751d75b1df9078fe6f907e
Marisol Y. Hernandez <68923545+themarisolhernandez@users.noreply.github.com>
2024-08-02 22:26:10 +0000

    My simply streamlit app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,29 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+from pickle import load
+import pandas as pd
 
-# your code here
+pipeline = load(open("trained_pipeline.pkl", "rb"))
+
+st.title("Iris - Model prediction")
+
+val1 = st.slider("Sepel length (cm)", min_value = 0.0, max_value = 4.0, step = 0.1)
+val2 = st.slider("Sepel width (cm)", min_value = 0.0, max_value = 4.0, step = 0.1)
+val3 = st.slider("Petal legnth (cm)", min_value = 0.0, max_value = 4.0, step = 0.1)
+val4 = st.slider("Petal width (cm)", min_value = 0.0, max_value = 4.0, step = 0.1)
+
+if st.button("Predict"):
+    # New data point for prediction (example with some missing values)
+    new_data_point_dict = {
+        'sepal length (cm)': [val1],
+        'sepal width (cm)': [val2],
+        'petal length (cm)': [val3],
+        'petal width (cm)': [val4]
+    }
+
+    # Convert the dictionary to a DataFrame
+    new_data_point_df = pd.DataFrame(new_data_point_dict)
+
+    # Pipeline makes a class prediction
+    pred_class = pipeline.predict(new_data_point_df)[0]
+    st.write("Prediction:", pred_class)
+
